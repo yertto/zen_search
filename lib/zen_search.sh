5a9a92cb0b4de2f68cef18eec134c36862c3f99f
yertto <yertto@gmail.com>
2018-12-31 11:09:03 +1100

    Add `show()`

--- lib/zen_search.sh
+++ lib/zen_search.sh
@@ -2,5 +2,13 @@
 DATA_DIR=${DATA_DIR:-./data}
 
 keys_for() { local repository="$1"
-  jq --raw-output 'first | keys_unsorted | join(" ")' "${DATA_DIR}/$1.json"
+  jq --raw-output 'first | keys_unsorted | join(" ")' "$(json_file "${repository}")"
+}
+
+show() { local repository="$1"; local _id="$2"
+  jq ".[] | select(._id==$_id)" "$(json_file "${repository}")"
+}
+
+json_file() { local repository="$1"
+  echo "${DATA_DIR}/${repository}.json"
 }
--- shpec/zen_search_shpec.sh
+++ shpec/zen_search_shpec.sh
@@ -17,5 +17,42 @@ describe "zen_search"
       assert equal "$(keys_for organizations)" "_id url external_id name domain_names created_at details shared_tickets tags"
     end
   end
+
+  describe "show"
+    describe "users"
+      it "returns human readable json"
+        actual="$(show users 1)"
+        expected=$(cat <<EOF
+{
+  "_id": 1,
+  "url": "http://initech.zendesk.com/api/v2/users/1.json",
+  "external_id": "74341f74-9c79-49d5-9611-87ef9b6eb75f",
+  "name": "Francisca Rasmussen",
+  "alias": "Miss Coffey",
+  "created_at": "2016-04-15T05:19:46 -10:00",
+  "active": true,
+  "verified": true,
+  "shared": false,
+  "locale": "en-AU",
+  "timezone": "Sri Lanka",
+  "last_login_at": "2013-08-04T01:03:27 -10:00",
+  "email": "coffeyrasmussen@flotonic.com",
+  "phone": "8335-422-718",
+  "signature": "Don't Worry Be Happy!",
+  "organization_id": 119,
+  "tags": [
+    "Springville",
+    "Sutton",
+    "Hartsville/Hartley",
+    "Diaperville"
+  ],
+  "suspended": true,
+  "role": "admin"
+}
+EOF)
+        assert equal "$actual" "$expected"
+      end
+    end
+  end
 end
 
